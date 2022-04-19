48fa2667b7c654e723f6c329066bc554d3cb7c76
Oli <o.radlett@gmail.com>
2022-04-19 11:10:42 +0100

    Run ur3 tests

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,8 @@ public class Main {
 		tests.test_basic_1();
 		System.out.println("---------------------------");
 		tests.test_ur2();
+		System.out.println("---------------------------");
+		tests.test_ur3();
 
 	}
 
